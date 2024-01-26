824b8ca1dc11ca3803549baa14786cfb5f062b12
Jemma Crosby <jemma.crosby37@thehutgroup.com>
2024-01-26 17:13:10 +0000

    trying again

--- src/main/java/com/thg/accelerator23/connectn/ai/jemma/Moves/BasicMoves.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/jemma/Moves/BasicMoves.java
@@ -48,17 +48,16 @@ public class BasicMoves {
         return availableColumns.get(randomIndex);
     }
 
-    public int checkDiagonal(Counter counter){
+    public int checkDiagonal(Counter counter) {
         for (int col = 0; col < 10; col++) {
-            for (int row = 0; row < 8; row++) {
+            for (int row = 0; row < 5; row++) {
                 if (col < 7 && row < 5 && counterPositions[row][col] != null &&
                         counterPositions[row][col].equals(counter) &&
                         counterPositions[row + 1][col + 1] != null &&
                         counterPositions[row + 1][col + 1].equals(counter) &&
                         counterPositions[row + 2][col + 2] != null &&
                         counterPositions[row + 2][col + 2].equals(counter) &&
-                        counterPositions[row + 3][col + 3] == null &&
-                        counterPositions[row + 3][col + 2] != null) {
+                        (row + 3 == 8 || counterPositions[row + 3][col + 3] == null)) {
                     return col + 3;
                 }
             }
@@ -66,15 +65,14 @@ public class BasicMoves {
 
         // Check top-right to bottom-left diagonal
         for (int col = 3; col < 10; col++) {
-            for (int row = 0; row < 8; row++) {
+            for (int row = 0; row < 5; row++) {
                 if (col > 2 && row < 5 && counterPositions[row][col] != null &&
                         counterPositions[row][col].equals(counter) &&
                         counterPositions[row + 1][col - 1] != null &&
                         counterPositions[row + 1][col - 1].equals(counter) &&
                         counterPositions[row + 2][col - 2] != null &&
                         counterPositions[row + 2][col - 2].equals(counter) &&
-                        counterPositions[row + 3][col - 3] == null &&
-                        counterPositions[row + 3][col - 2] != null) {
+                        (row + 3 == 8 || counterPositions[row + 3][col - 3] == null)) {
                     return col - 2;
                 }
             }
@@ -82,16 +80,16 @@ public class BasicMoves {
         return -1;
     }
 
-    public int checkHorizontal(Counter counter){
-        for (int col = 0; col < 10; col++) {
+    public int checkHorizontal(Counter counter) {
+        for (int col = 0; col < 7; col++) {
             for (int row = 0; row < 8; row++) {
-                if (col < 7 && counterPositions[row][col] != null &&
+                if (counterPositions[row][col] != null &&
                         counterPositions[row][col].equals(counter) &&
                         counterPositions[row][col + 1] != null &&
                         counterPositions[row][col + 1].equals(counter) &&
                         counterPositions[row][col + 2] != null &&
                         counterPositions[row][col + 2].equals(counter) &&
-                        counterPositions[row][col + 3] == null){
+                        (col + 3 == 10 || counterPositions[row][col + 3] == null)) {
                     return col + 3;
                 }
             }
@@ -99,31 +97,32 @@ public class BasicMoves {
         return -1;
     }
 
-//    public int checkVertical(Counter counter){
-//        for (int col = 0; col < 10; col++) {
-//            for (int row = 0; row < 5; row++) {
-//                if (counterPositions[row][col] != null &&
-//                    counterPositions[row][col].equals(counter) &&
-//                    counterPositions[row + 1][col] != null &&
-//                    counterPositions[row + 1][col].equals(counter) &&
-//                    counterPositions[row + 2][col] != null &&
-//                    counterPositions[row + 2][col].equals(counter) &&
-//                    counterPositions[row + 3][col] == null){
-//                    return col;
-//                }
-//            }
-//        }
-//        return -1;
-//    }
+    public int checkVertical(Counter counter) {
+        for (int col = 0; col < 10; col++) {
+            for (int row = 0; row < 5; row++) {
+                if (counterPositions[row][col] != null &&
+                        counterPositions[row][col].equals(counter) &&
+                        counterPositions[row + 1][col] != null &&
+                        counterPositions[row + 1][col].equals(counter) &&
+                        counterPositions[row + 2][col] != null &&
+                        counterPositions[row + 2][col].equals(counter) &&
+                        (row + 3 == 8 || counterPositions[row + 3][col] == null)) {
+                    return col;
+                }
+            }
+        }
+        return -1;
+    }
+
 
     public int movePosition(Counter counter){
-//        int vertical = checkVertical(counter);
+        int vertical = checkVertical(counter);
         int horizontal = checkHorizontal(counter);
         int diagonal = checkDiagonal(counter);
 
-//        if(vertical != -1 && vertical < 10){
-//            return vertical;
-//        }else
+        if(vertical != -1 && vertical < 10){
+            return vertical;
+        }else
         if(horizontal != -1 && horizontal < 10){
             return horizontal;
         }else if(diagonal != -1 && diagonal <10){
